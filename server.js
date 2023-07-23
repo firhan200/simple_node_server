6593acdc9801c4d1fbf3f1524995a432e911ab4e
firhan faisal <firhan@jmdigital.id>
2023-07-23 20:42:03 +0700

    fix main yaml and add changes on server js

--- server.js
+++ server.js
@@ -3,7 +3,11 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-    res.send('Another changes...')
+    res.send('<h1>Amazing</h1>')
+})
+
+app.get('/about', (req, res) => {
+    res.send('<h1>About</h1>')
 })
 
 app.listen(port, () => {
